7ecaf473508a31c9ae59e9581e8110e3c127ace4
Stuart Olivera <stuart@stuartolivera.com>
2014-06-10 19:45:10 -0400

    Added a single player & interface

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 (function(){
   var app = angular.module('arit', [ ]);
+
+  app.controller('PlayerController', function() {
+    this.player = player;
+  });
+
+  var player = {
+    name: 'John Doe',
+    description: 'Tall. Very tall.',
+    attacked: false
+  }
+
 })();

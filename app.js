7d4a1d5cdd2c9c1ee7adee848c7657a7b84e8031
Stuart Olivera <stuart@stuartolivera.com>
2014-06-10 19:54:23 -0400

    Added kills and deaths to each player

--- app.js
+++ app.js
@@ -9,11 +9,15 @@
     {
       name: 'John Doe',
       description: 'Tall. Very tall.',
+      kills: 10,
+      deaths: 3,
       attacked: false
     },
     {
       name: 'Joe Smith',
       description: 'Quite average, wears glasses.',
+      kills: 5,
+      deaths: 9,
       attacked: true
     }
   ];
